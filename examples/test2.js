6fdcc28fd7180d0792c1b84ecb488b7df2a8e59b
Ethan Anderson <aethan@uw.edu>
2016-01-20 15:24:07 -0800

    add examples

---
+++ examples/test2.js
@@ -0,0 +1,4 @@
+var chalk = require('chalk');
+var yosay = require('../');
+
+console.log(yosay(chalk.red('WHAT DOES THE YO SAY??? ') + chalk.yellow('\'ALLO \'ALLO')));
---
+++ examples/test3.js
@@ -0,0 +1,2 @@
+var yosay = require('yosay');
+console.log(yosay("Yo say yo say yo say"), {maxLength:100});
